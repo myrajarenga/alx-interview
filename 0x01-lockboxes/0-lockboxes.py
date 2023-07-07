2cc386793860fc110d60392f9a5d6f428c829343
root <myrajarenga1234@gmail.com>
2023-07-07 10:29:57 +0000

    lock boxes interview qn

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,32 +3,28 @@
 """
 lock-boxes
 """
+
+
 def unlocker(box, openboxes, boxes):
     """Recursive function to unlock the boxes"""
     if len(boxes) == len(openboxes):
-        # If all boxes are already open, return the set of open boxes
         return openboxes
     for value in box:
         if value >= len(boxes):
             continue
         if value not in openboxes:
-            # If the box is not already open, add it to the set of open boxes
             openboxes.add(value)
-            # Recursively call the unlocker function for the next box
             unlocker(boxes[value], openboxes, boxes)
     return openboxes
 
 
 def canUnlockAll(boxes):
-    # Main function to check if all boxes can be unlocked
-    openboxes = {0}  # Start with the first box open
+    """ Main function to check if all boxes can be unlocked"""
+    openboxes = {0}
     if len(boxes) == 1:
-        # If there is only one box, it is already unlocked
         return True
     unlocked = unlocker(boxes[0], openboxes, boxes)
     if len(unlocked) == len(boxes):
-        # If the number of open boxes is equal to the total number of boxes, return True
         return True
     else:
-        # Otherwise, return False
         return False
