d11eeb37aac557f40fbdd236ce13cb5c06e3cb7e
root <myrajarenga1234@gmail.com>
2023-07-12 08:44:51 +0000

    minimum operation interiew question

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+minimum operations
+"""
+
+
+def minOperations(n):
+    """ return number of minimal operations"""
+    if n < 2:
+        return 0
+
+    operations = 0
+    current = 1
+
+    while current != n:
+        if n % current == 0:
+            operations += 1
+            current *= 2
+
+            if current == n:
+                break
+        operations += 1
+        current += current
+
+    return operations
