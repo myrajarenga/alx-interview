a60e1e4dfa9d925fa8f8326a3117e68525417459
myrajarenga <myrajarenga1234@gmail.com>
2023-07-20 13:28:19 -0700

    wrting a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,42 +9,38 @@ import sys
 def compute_metrics():
     code = {}
     file_size = 0
-    line_count = 0
+    vals_total = 0
 
     try:
+        print_status_codes(code, file_size)
+
         for line in sys.stdin:
-            line = line.strip()
+            line = line.split()
+            file_size += int(line[-1])
+            status = line[-2]
+            vals = list(code.values())
+            vals_total = sum(vals)
 
-            if not line:
-                """If the line is empty"""
-                print_statistics(code, file_size)
-                return
+            if status not in code:
+                code[status] = 1
+            else:
+                code[status] += 1
 
-            """Split the line using double quotes as the separator"""
-            parts = line.split('"')
+            vals_total += 1
 
-            if len(parts) >= 3:
-                """Extract the file size from the last part"""
-                size = int(parts[-1].split()[-1])
-                file_size += size
-
-                """Extract the status code from the middle part"""
-                status_code = parts[1].split()[-2]
-
-                code[status_code] = code.get(status_code, 0) + 1
-                line_count += 1
-
-                if line_count % 10 == 0:
-                    print_statistics(code, file_size)
+            if vals_total % 10 == 0:
+                print_status_codes(code, file_size)
 
     except KeyboardInterrupt:
-        print_statistics(code, file_size)
+        print("\nProgram interrupted. Current statistics:")
+        print_status_codes(code, file_size)
+        sys.exit(0)
 
 
-def print_statistics(code, file_size):
-    print("file size:", file_size)
-    for status_code, count in sorted(code.items()):
-        print(f"{status_code}: {count}")
+def print_status_codes(code, file_size):
+    print("File size:", file_size)
+    for status, count in sorted(code.items()):
+        print(f"{status}: {count}")
     print()
 
 
