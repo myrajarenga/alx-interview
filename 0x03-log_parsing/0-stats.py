daf1f0c06790e83aa875dbe031b3d682d2f5741e
myrajarenga <myrajarenga1234@gmail.com>
2023-07-20 08:16:30 -0700

    script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,47 +8,43 @@ import sys
 import re
 
 
-def compute_and_print_stats(total_size=0, status_code_count=None, line_count=0):
-    """function to computr statistics"""
+def compute_metrics_recursive(total_size=0, status_code_count=None, line_count=1):
+    """function to compute metix using recursion"""
     if status_code_count is None:
         status_code_count = {}
 
     try:
-        for line in sys.stdin:
-            match = re.match(
-                r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)',
-                line
-            )
-
-            if match:
-                ip_address, date, status_code, file_size = match.groups()
-                total_size += int(file_size)
-
-                if status_code.isdigit():
-                    status_code_count[int(status_code)] =\
-                            status_code_count.get(int(status_code), 0) + 1
-
-            line_count += 1
-
-            if line_count % 10 == 0:
-                print_statistics(total_size, status_code_count)
-
+        line = next(sys.stdin)
+    except StopIteration:
         print("\nEnd of input. Final statistics:")
         print_statistics(total_size, status_code_count)
+        return
 
-    except KeyboardInterrupt:
-        """If interrupted by Ctrl + C, print the current statistics and exit"""
+    match = re.match(
+        r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)',
+        line
+    )
+
+    if match:
+        ip_address, date, status_code, file_size = match.groups()
+        total_size += int(file_size)
+
+        if status_code.isdigit():
+            status_code_count[int(status_code)] = status_code_count.get(int(status_code), 0) + 1
+
+    if line_count % 10 == 0:
         print_statistics(total_size, status_code_count)
-        sys.exit(0)
+
+    compute_metrics_recursive(total_size, status_code_count, line_count + 1)
 
 
 def print_statistics(total_size, status_code_count):
-    """print total statistics"""
-    print("File size:", total_size)
+    """print statistics"""
+    print("Total file size:", total_size)
     for status_code in sorted(status_code_count.keys()):
         print(f"{status_code}: {status_code_count[status_code]}")
     print()
 
 
 if __name__ == "__main__":
-    compute_and_print_stats()
+    compute_metrics_recursive()
