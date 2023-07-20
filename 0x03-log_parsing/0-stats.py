a15360afbe4ab309a06011551737403c001bc0ef
myrajarenga <myrajarenga1234@gmail.com>
2023-07-20 08:57:45 -0700

    script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,47 +8,44 @@ import re
 
 
 def compute_metrics_recursive(file_sizes=0, status_counts=None, line_count=0):
-    """function to compute metrix using recursion"""
+    """function that computes metix using recursion"""
     if status_counts is None:
         status_counts = {}
 
     try:
-        line = input().strip()  # Read a line from the standard input
+        for line in sys.stdin:
+            line = line.strip()
 
-        if not line:
-            print_statistics(file_sizes, status_counts)
-            return
+            if not line:
+                print_statistics(file_sizes, status_counts)
+                return
 
-        # Use regular expression to match the input format
-        match = re.match(
-            r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)',
-            line
-        )
+            match = re.match(
+                r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)',
+                line
+            )
 
-        if match:
-            _, _, status_code, file_size = match.groups()
-            file_sizes += int(file_size)
+            if match:
+                _, _, status_code, file_size = match.groups()
+                file_sizes += int(file_size)
 
-            if status_code.isdigit():
-                status_counts[int(status_code)]\
-                        = status_counts.get(int(status_code), 0) + 1
+                if status_code.isdigit():
+                    status_counts[int(status_code)]\
+                            = status_counts.get(int(status_code), 0) + 1
 
-        line_count += 1
+            line_count += 1
 
-        if line_count % 10 == 0:
-            print_statistics(file_sizes, status_counts)
-
-        compute_metrics_recursive(file_sizes, status_counts, line_count)
+            if line_count % 10 == 0:
+                print_statistics(file_sizes, status_counts)
 
     except KeyboardInterrupt:
-        """Program interrupted. Current statistics"""
         print_statistics(file_sizes, status_counts)
         sys.exit(0)
 
 
 def print_statistics(file_sizes, status_counts):
-    """total ststistics"""
-    print("Total file size:", file_sizes)
+    """prin statistics"""
+    print("file size", file_sizes)
     for status_code in sorted(status_counts.keys()):
         print(f"{status_code}: {status_counts[status_code]}")
     print()
---
+++ 0x03-log_parsing/status.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+script that reads stdin by line
+"""
+
+
+import sys
+import re
+
+
+def compute_metrics():
+    """function to compuute metrix"""
+    total_size = 0
+    status_code_count = {}
+
+    try:
+        for line_count, line in enumerate(sys.stdin, start=1):
+            """ Use regular expression to match the input format"""
+            match = re.match(
+                (r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" '
+                 r'(\d{3}) (\d+)'),
+                line
+            )
+            if match:
+                ip_address, date, status_code, file_size = match.groups()
+                total_size += int(file_size)
+
+                if status_code.isdigit():
+                    status_code_count[int(status_code)]\
+                            = status_code_count.get(int(status_code), 0) + 1
+
+            if line_count % 10 == 0:
+                print_statistics(total_size, status_code_count)
+
+    except KeyboardInterrupt:
+        print("\nProgram interrupted. Final statistics:")
+        print_statistics(total_size, status_code_count)
+
+
+#def print_statistics(total_size, status_code_count):
+ #   print("Total file size:", total_size)
+  #  for status_code in sorted(status_code_count.keys()):
+   #     print(f"{status_code}: {status_code_count[status_code]}")
+   # print()
+
+
+if __name__ == "__main__":
+    compute_metrics()
