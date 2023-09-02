f7974b38a1c6829c2526c9297e94297329ded77c
myrajarenga <myrajarenga1234@gmail.com>
2023-09-02 10:02:47 +0000

    island perimeter calculation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,38 +8,23 @@ def island_perimeter(grid):
     if not grid:
         return 0
 
+    perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
-    perimeter = 0
 
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                # Check the four neighboring cells
-                up = row - 1
-                down = row + 1
-                left = col - 1
-                right = col + 1
+                perimeter += 4  # Assuming all sides are land
 
-                # Initialize edge count
-                edges = 0
-
-                # Check up
-                if up < 0 or grid[up][col] == 0:
-                    edges += 1
-
-                # Check down
-                if down >= rows or grid[down][col] == 0:
-                    edges += 1
-
-                # Check left
-                if left < 0 or grid[row][left] == 0:
-                    edges += 1
-
-                # Check right
-                if right >= cols or grid[row][right] == 0:
-                    edges += 1
-
-                perimeter += edges
+                # Check each adjacent cell and subtract 1 for each neighboring land cell
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
 
     return perimeter
