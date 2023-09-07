3776489a3921846bf930e6f3750c1a1d4ef79f80
myrajarenga <myrajarenga1234@gmail.com>
2023-09-07 11:26:51 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,6 +6,15 @@ prime numbers
 """
 
 
+def generate_primes(n):
+    """function to generaat eprime numbers"""
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            primes[i*i : n+1 : i] = [False] * len(primes[i*i : n+1 : i])
+    return primes
+
 def isWinner(x, nums):
     """
     Determines the winner of multiple rounds of a game based on prime number selection.
@@ -17,25 +26,14 @@ def isWinner(x, nums):
     Returns:
         str or None: Name of the player with the most wins. Returns None if the winner cannot be determined.
     """
-
     if x < 1 or not nums:
         return None
 
     marias_wins, bens_wins = 0, 0
 
-    def generate_primes(n):
-        primes = [True for _ in range(1, n + 1, 1)]
-        primes[0] = False
-        for i, is_prime in enumerate(primes, 1):
-            if i == 1 or not is_prime:
-                continue
-            for j in range(i + i, n + 1, i):
-                primes[j - 1] = False
-        return primes
-
     for _, n in zip(range(x), nums):
         primes = generate_primes(n)
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        primes_count = sum(primes[2:n+1])
         bens_wins += primes_count % 2 == 0
         marias_wins += primes_count % 2 == 1
 
